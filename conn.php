4c21e05612e2a062b8c1061bcf5cba9d8f3c47ca
ElmaSakic <sakicelma4@gmail.com>
2018-05-27 15:49:11 +0200

    more changes

--- conn.php
+++ conn.php
@@ -4,7 +4,7 @@ $username = "id5956478_elma";
 $password = "2161997";
 
 try {
-    $conn = new PDO("mysql:host=$servername;dbname='id5871651_projekat'", $username, $password);
+    $conn = new PDO("mysql:host=$servername;dbname='id5956478_projekat'", $username, $password);
     // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     echo "Connected successfully";
